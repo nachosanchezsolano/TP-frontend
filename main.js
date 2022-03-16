3b9a41814460b7d96605fb59dbf7179a1422006f
nachosanchezsolano <nacho.sanchezsolano@gmail.com>
2022-03-16 08:25:12 -0300

    16/3

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function localStorageSaveCombo() {   
+var combo=[];
+var name=document.getElementById("clientName").value;
+var pizzaType=document.getElementById("pizzaType").value;
+var pizaSize=document.getElementById("pizzaSize").value;
+    console.log(clientName)
+}
