64840e4b1461288e93ffe78e2ab7a53ddb42bd8f
nachosanchezsolano <77471637+nachosanchezsolano@users.noreply.github.com>
2022-03-16 12:42:37 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,5 +4,8 @@ var combo=[];
 var name=document.getElementById("clientName").value;
 var pizzaType=document.getElementById("pizzaType").value;
 var pizaSize=document.getElementById("pizzaSize").value;
-    console.log(clientName)
+   combo.push(name,pizzaType,pizaSize)
+
+
+ return combo
 }
